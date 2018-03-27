0fb5c4dafbd591828757ded988092b78e88bbf98
Liszt Louis <lisztlouis@gmail.com>
2018-03-27 14:49:02 -0400

    added my own branch

---
+++ src/components/anime.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {render} from 'react-dom';
+class Anime extends React.Component {
+  render(){
+    return (
+      <div>
+        //Enter your data here
+      </div>
+    )
+  }
+}
+export default Anime
