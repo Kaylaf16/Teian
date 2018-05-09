60e1f7812105a8fd39516a0d644d06e700a06eab
Liszt Louis <lisztlouis@gmail.com>
2018-05-08 20:58:43 -0400

    fixed index.json

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/GetAll',require('./GetAll.js'))
-router.use('/GetByGenre',require('./GetByGenre.js'))
-router.use('/GetById',require('./GetById.js'))
 
+router.use('/GetByGenre',require('./GetByGenre.js'))
 router.use('/animes', require('./animes.js'))
 module.exports=router;
