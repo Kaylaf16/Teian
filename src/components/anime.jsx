8a6ff5694567f4ccc79584fcaa021c2d44183113
Liszt Louis <lisztlouis@gmail.com>
2018-05-15 20:56:04 -0400

    Fixing mongo

--- src/components/anime.jsx
+++ src/components/anime.jsx
@@ -2,14 +2,45 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Container, Row, Col, Button} from 'reactstrap';
 import Header from './header.jsx'
-import AnimeDescription from './animedescr.jsx'
+import anime from '../../public/oneanime.json'
 //import AnimeCard from './animecard.jsx'
 
+
 class Anime extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render(){
+
+    var RowStyle = {
+      top: '50%',
+    }
+
     return(
     <div>
-      <AnimeDescription />
+      <Header />
+      <Container>
+        <div style={RowStyle}>
+          <Row>
+            <Col md="9" offset="2">
+              <h1> {anime.result[0].title} </h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md="3">
+              <img src={anime.result[0].image_url} />
+            </Col>
+            <Col md = "9">
+              <p>
+                {anime.result[0].synopsis}
+              </p>
+            </Col>
+
+          </Row>
+        </div>
+      </Container>
     </div>
     )
   }
